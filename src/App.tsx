4e0708929a867f483841edab5a83056a1403d3b2
usmansbk <usmansbk@gmail.com>
2022-06-30 11:14:47 +0100

    add suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { Suspense } from "react";
+import { Toaster } from "react-hot-toast";
+import Sentry from "config/sentry";
+import i18n from "config/i18n";
+
 function App() {
-  return <h1>Client</h1>;
+  return (
+    <Suspense fallback={<h1>Loading...</h1>}>
+      <h1>Hello</h1>
+      <Toaster />
+    </Suspense>
+  );
 }
 
-export default App;
+export default Sentry.withErrorBoundary(App, {
+  fallback: i18n.t("Something went wrong"),
+});
