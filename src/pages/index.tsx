02633255f5de6a4c235b34623f03e807dff25774
usmansbk <usmansbk@gmail.com>
2022-06-30 14:59:23 +0100

    add routes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,6 +11,32 @@ import PublicLayout from "layouts/PublicLayout";
 import { useAuthState } from "api/graphql/hooks/app";
 import routes from "./routes";
 
+const authRoutes = [
+  {
+    path: routes.login,
+    element: <h1>Login</h1>,
+  },
+  {
+    path: routes.register,
+    element: <h1>Register</h1>,
+  },
+  {
+    path: routes.forgotPassword,
+    element: <h1>Forgot Password</h1>,
+  },
+  {
+    path: routes.resetPassword,
+    element: <h1>Reset Password</h1>,
+  },
+];
+
+const publicRoutes = [
+  {
+    path: routes.verifyEmail,
+    element: <h1>Verify Email</h1>,
+  },
+];
+
 function Protected() {
   const location = useLocation();
   const { isLoggedIn } = useAuthState();
@@ -39,22 +65,17 @@ function Pages() {
       <Routes>
         <Route element={<RootLayout />}>
           <Route element={<Auth />}>
-            <Route path={routes.login} element={<h1>Login</h1>} />
-            <Route path={routes.register} element={<h1>Register</h1>} />
-            <Route
-              path={routes.forgotPassword}
-              element={<h1>Forgot Password</h1>}
-            />
-            <Route
-              path={routes.resetPassword}
-              element={<h1>Reset Password</h1>}
-            />
+            {authRoutes.map(({ path, element }) => (
+              <Route key={path} path={path} element={element} />
+            ))}
           </Route>
           <Route path={routes.home} element={<Protected />}>
             <Route index element={<h1>Main App</h1>} />
           </Route>
           <Route element={<PublicLayout />}>
-            <Route path={routes.verifyEmail} element={<h1>Verify Email</h1>} />
+            {publicRoutes.map(({ path, element }) => (
+              <Route key={path} path={path} element={element} />
+            ))}
           </Route>
         </Route>
       </Routes>
